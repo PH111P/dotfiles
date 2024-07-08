988dfaccb609e7ef3287998263fdb81ea685c7d2
Philip Wellnitz [10] <philipwellnitz@gmx.de>
2024-07-08 13:41:34 +0900

    ai3

---
+++ themer/Ai3/config.fish
@@ -0,0 +1,62 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# fish config
+set fish_greeting
+
+alias python='python3'
+
+# Powerline
+set PYTHON_SITE_PATH (python3 -c "import site; print(site.getsitepackages()[0])")
+source "$PYTHON_SITE_PATH/powerline/bindings/fish/powerline-setup.fish"
+powerline-setup
+
+function fish_vi_cursor
+end
+
+# some colors
+set fish_color_autosuggestion dad9d5
+set fish_color_comment        522829
+set fish_color_command        dad9d5 --bold
+set fish_color_valid_path     dad9d5 --bold
+set fish_color_error          c7bbc7 --bold
+set fish_color_param          62615f
+
+# Some important exports
+set -gx TERM xterm-256color
+set -gx EDITOR vim
+
+set -gx DEVKITPRO /opt/devkitpro
+set -gx DEVKITARM /opt/devkitpro/devkitARM
+
+# some alias
+
+alias g++='g++-13 -std=c++23 -O2 -static -w'
+
+alias Update='brew update; and brew upgrade'
+alias Install='brew install'
+alias Search='brew search'
+alias Remove='brew remove'
+
+alias π='pdfpc'
+
+alias Pb='python3 setup.py build'
+alias Pi='sudo python3 setup.py install'
+
+alias σ='sudo systemctl restart tor'
+
+alias タ='youtube-dl -x --audio-format mp3 --audio-quality 0'
+alias た='θ -o "%(playlist_index)s - %(title)s.%(ext)s"'
+
+alias :q='exit'
+
+alias cal='cal -m'
+
+alias mpi='sudo openconnect sslvpn.mpi-klsb.mpg.de'
+
+alias calc='ipython'
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+export LANG="ja_JP.UTF8"
